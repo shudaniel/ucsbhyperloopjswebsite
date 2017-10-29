74fc5a60e773af6a6e2fca3dcd078ae4ec645f89
Nicolas Toscano <nicolas.toscano@gmail.com>
2017-10-29 12:48:29 -0700

    pushing material import example

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        <AppBar>
+        <Toolbar>
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
-        </header>
+          </Toolbar>
+        </AppBar>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
