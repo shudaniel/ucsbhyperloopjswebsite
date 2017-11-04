3e6648e076d792478c856e156a7ef943b7f4b23f
Mark Wu <markwu35@yahoo.com>
2017-11-04 14:51:56 -0700

    fixed express server code

--- server/app.js
+++ server/app.js
@@ -5,11 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve static assets
-app.use(express.static(path.resolve(__dirname, '..', 'build')));
+app.use(express.static(__dirname + './../build'));
+app.use("/assets", express.static(__dirname + './../assets'));
 
 // Always return the main index.html, so react-router render the route in the client
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
-});
+
 
 module.exports = app;
