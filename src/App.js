f4ef1d0a739678510389a973ab39da5bd79a7d36
Daniel Shu <danielshu98@gmail.com>
2017-11-12 15:30:22 -0800

    Updated homepage css and added stub sections

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
   render() {
     return (
       <div>
-      <NavBar />
+        <NavBar />
         <Carousel showThumbs={false} infiniteLoop={true}>
           <div>
             <img src="assets/ucsbhyp.jpg"/>
@@ -26,7 +26,8 @@ class App extends Component {
         </Carousel>
         <div className="main-body">
           <div className="bigblock" id="pod">
- 
+            <h1>Our Pod</h1><br />
+
             <StaggeredCard image="assets/cat.jpg" index="0" title="Section Title"
                         text="Lighter than dry pasta and deeply flavorful, fresh pasta isn't something to master in one go, but the end result is truly worthwhile. This particular recipe for an egg dough is vastly versatile. It can take on flavors from whole grain flours, saffron, herbs, or sauteed greens, or can be shaped into a number of different styles. And, with time and practice, what may have started as a cooking project may end up being the foundation for weeknight meals to come."
             />
@@ -38,7 +39,6 @@ class App extends Component {
             />
           </div>
           <div className="bigblock" id="team">
-            team
             <h1>Meet the Team</h1><br/>
             <Profiles team={Mechanical} title="Mechanical Engineers"
             />
@@ -46,13 +46,19 @@ class App extends Component {
             />
           </div>
           <div className="bigblock" id="sponsors">
-            asdfdfs
+            <h1>Our Sponsors</h1><br />
           </div>
           <div className="bigblock" id="competition">
-            asdfa
+            <h1>The Hyperloop Competition</h1><br />
+            <StaggeredCard image="assets/cat.jpg" index="0" title="Section Title"
+                        text="Lighter than dry pasta and deeply flavorful, fresh pasta isn't something to master in one go, but the end result is truly worthwhile. This particular recipe for an egg dough is vastly versatile. It can take on flavors from whole grain flours, saffron, herbs, or sauteed greens, or can be shaped into a number of different styles. And, with time and practice, what may have started as a cooking project may end up being the foundation for weeknight meals to come."
+           />
           </div>
           <div className="bigblock" id="supportUs">
-            asdfasd
+            <h1>Support Us</h1><br />
+            <StaggeredCard image="assets/cat.jpg" index="1" title="Section Title"
+                text="Lighter than dry pasta and deeply flavorful, fresh pasta isn't something to master in one go, but the end result is truly worthwhile. This particular recipe for an egg dough is vastly versatile. It can take on flavors from whole grain flours, saffron, herbs, or sauteed greens, or can be shaped into a number of different styles. And, with time and practice, what may have started as a cooking project may end up being the foundation for weeknight meals to come."
+            />
           </div>
         </div>
       </div>
