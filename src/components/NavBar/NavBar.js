8912cf74a73bc0f0cedc5c867d137d77a45e7d52
Daniel Shu <danielshu98@gmail.com>
2017-11-17 14:48:51 -0800

    Made navbar scale with window size

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './NavBar.css';
 
-import { AppBar, Tabs, Tab, Toolbar, ToolbarGroup, FlatButton } from 'material-ui';
+import { AppBar, Tabs, Tab, Toolbar, ToolbarGroup, IconMenu, IconButton, MenuItem} from 'material-ui';
+import MediaQuery from 'react-responsive';
+import MoreHorizIcon from 'material-ui/svg-icons/navigation/more-horiz';
 
 
 class NavBar extends Component{
@@ -20,11 +22,29 @@ class NavBar extends Component{
                 <Tab className="navbar-tab" label="Gallery" href="#gallery" />
             </Tabs>
         );
+        var iconTabs = (
+            <IconMenu iconButtonElement={<IconButton><MoreHorizIcon color="white" /> </IconButton>}>
+                <MenuItem primaryText="Pod" href="#pod" />
+                <MenuItem primaryText="Team" href="#team" />
+                <MenuItem primaryText="Sponsors" href="#sponsors" />
+                <MenuItem primaryText="Support Us" href="#supportUs" />
+                <MenuItem primaryText="Gallery" href="#gallery" />
+
+            </IconMenu>
+        );
+
         return (
             <AppBar showMenuIconButton={false}
                 title={<a href="#top"><img src="assets/logo-inverted.png" height = "50px"/> </a>}
                 iconElementRight={
-                    <ToolbarGroup style={{ width: "100%", backgroundColor: "inherit" }}>{myTabs}</ToolbarGroup>
+                    <div>
+                        <MediaQuery query="(min-width: 610px)">
+                            <ToolbarGroup style={{ width: "100%", backgroundColor: "inherit" }}>{myTabs}</ToolbarGroup>
+                        </MediaQuery>
+                        <MediaQuery query="(max-width: 610px)">
+                            {iconTabs}
+                        </MediaQuery>
+                    </div>
                 }
                 
                 style={{ position: "fixed", backgroundColor: "#18335d" }}>
